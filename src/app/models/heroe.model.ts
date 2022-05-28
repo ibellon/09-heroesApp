55bb0c985c9e26bb320628ae72974197a41df121
ibellon <isidro.bellon@gmail.com>
2022-05-28 19:36:46 +0200

    Creando el modelo

---
+++ src/app/models/heroe.model.ts
@@ -0,0 +1,15 @@
+
+export class HeroeModel {
+
+    id: string;
+    nombre: string;
+    poder: string;
+    vivo: boolean;
+
+    constructor() {
+        this.id = "";
+        this.nombre = "";
+        this.poder = "";
+        this.vivo = true;
+    }
+}
