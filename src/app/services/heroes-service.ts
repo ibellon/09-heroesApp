754d6f17c4b7968e21ef1486e19d1e57b7849212
ibellon <isidro.bellon@gmail.com>
2022-06-09 18:02:12 +0200

    Obtener un registro

--- src/app/services/heroes-service.ts
+++ src/app/services/heroes-service.ts
@@ -43,6 +43,10 @@ export class HeroesService {
       );
   }
 
+  getHeroe(id: string) {
+    return this.http.get<HeroeModel>(this.url+"/heroes/"+id+".json");
+  }
+
   private crearObjeto(heroesObject: any) {
 
     const heroes: HeroeModel[] = [];
